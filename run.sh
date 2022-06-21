73dcfd2cff1bca301d4d1bc5daf7e80459badc5c
MH-Lee <lmhoon012@gmail.com>
2022-06-21 22:06:58 +0900

    file path debug

---
+++ run.sh
@@ -0,0 +1,3 @@
+python genome_preprocessing.py --vcf_path ../test --geno 0.01 --maf 0.05 --hwe 0.001 \
+                               --ld_windows 50 --ld_windows 5 --ld_r2 0.5 \
+                               --raw_dir ./lookup_table/ --output_dir ./csv_file --data_type 1k
