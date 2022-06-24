38948f7e985a23306b0b36817fa86340ccfbc3fa
MH-Lee <lmhoon012@gmail.com>
2022-06-24 15:24:19 +0900

    check path

--- command/korean_1k_run.sh
+++ command/korean_1k_run.sh
@@ -1,3 +1,3 @@
-python genome_preprocessing.py --vcf_path /data/imhoon012/notebook/shared/korean_1k_genome_data --geno 0.01 --maf 0.05 --hwe 0.001 \
+python genome_preprocessing.py --vcf_path ./korean_1k_genome_data --geno 0.01 --maf 0.05 --hwe 0.001 \
                                --ld_windows 50 --ld_windows 5 --ld_r2 0.5 --plink_path ./korean_1k/plink_1k \
                                --raw_path ./korean_1k/lookup_table --output_path ./korean_1k/csv_file --data_type 1k
